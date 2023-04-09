a9f36c74c089d16102d328ee6dce7ed56a306a83
Taleem <taleemmankuer@gmail.com>
2023-04-09 10:56:01 +0300

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,13 @@ export const metadata = {
 };
 import "../styles/globals.css";
 import Footer from "@/components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Analytics />
         {children}
         <Footer />
       </body>
