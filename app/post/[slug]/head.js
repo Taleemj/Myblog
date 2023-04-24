1ba90d63dcc526b74670ecead5e7cb3c9f3ce1cb
Taleem <taleemmankuer@gmail.com>
2023-04-24 11:31:14 +0300

    update

--- app/post/[slug]/head.js
+++ app/post/[slug]/head.js
@@ -1,26 +1,26 @@
-import React from "react";
+import { groq } from "next-sanity";
+import { client } from "@/lib/sanity.client";
 
-const head = () => {
+const head = async ({ params: { slug } }) => {
+  const query = groq`
+  *[_type == "post" && slug.current == $slug][0]
+  {
+      ...,
+      author->,
+      categories[]->
+  }
+  `;
+  const post = await client.fetch(query, { slug });
+
   return (
     <>
-      <title>The developer Blog</title>
-      <meta
-        name="description"
-        content="the developer blog is a platform dedicated to sharing your knowledge, experience,
-         and insights on programming and technology-related topics. It aims to provide valuable
-         information and resources to fellow developers, beginners, and enthusiasts alike.
-         we covers a wide range of topics such as programming languages, web development,
-         software engineering, algorithms, data structures, best practices, and industry news.
-         It also features tutorials, code snippets, and practical examples that help readers understand
-         complex concepts and apply them in real-world scenarios. Your blog's style is approachable, informative,
-         and engaging, making it easy for readers to follow and learn. Whether you are a seasoned programmer
-         or a beginner, it offers something for everyone who is interested in the world of programming 
-         and technology"
-      />
+      <title>{post.title}</title>
+      <meta name="description" content={`${post.description}`} />
       <meta
         name="google-site-verification"
         content="O_grUoAkNqBhd8JaaX1YpzQO4ti2Njc6w_Oul2kjNRQ"
       />
+      <meta name="keyword" content={`${post.title}`} />
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8299193659017860"
--- app/post/[slug]/page.jsx
+++ app/post/[slug]/page.jsx
@@ -8,6 +8,7 @@ import urlFor from "@/lib/urlFor";
 import { PortableText } from "@portabletext/react";
 import { RichText } from "@/components/Richtext/RichText";
 import RelatedPosts from "@/components/RelatedPosts/RelatedPosts";
+import Head from "next/head";
 
 export const revalidate = 30;
 
@@ -54,6 +55,19 @@ const Post = async ({ params: { slug } }) => {
   );
   return (
     <>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={`${post.description}`} />
+        <meta
+          name="google-site-verification"
+          content="O_grUoAkNqBhd8JaaX1YpzQO4ti2Njc6w_Oul2kjNRQ"
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8299193659017860"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <main className={styles.container}>
         <nav className={styles.nav}>
           <div className={styles.navcontainer}>
@@ -100,6 +114,8 @@ const Post = async ({ params: { slug } }) => {
         </div>
         <div className={styles.routes}>
           <Link href="/">HOME</Link>
+          <p>/</p>
+          <p>{postcategory}</p>
           <p>/</p>
           <p>{post.slug.current}</p>
         </div>
