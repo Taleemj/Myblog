9fcead8b3bd650d3ae7e3f7fc521e4f30beec978
Taleem <taleemmankuer@gmail.com>
2023-05-18 13:42:32 +0300

    added contact section

---
+++ components/contact/Contact.jsx
@@ -0,0 +1,31 @@
+import styles from "./Contact.module.scss";
+import { BsArrowUp } from "react-icons/bs";
+
+const Contact = () => {
+  return (
+    <div className={styles.container}>
+      <BsArrowUp />
+      <h1>Let me know what you think, drop me a message </h1>
+      <form action="">
+        <input type="text" name="name" id="name" placeholder="Enter Name" />
+        <input
+          type="text"
+          name="subject"
+          id="subject"
+          placeholder="Enter Subject"
+        />
+        <input type="email" name="email" id="email" placeholder="Enter Email" />
+        <textarea
+          name="message"
+          id="message"
+          cols="25"
+          rows="7"
+          placeholder="Enter Message..."
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
