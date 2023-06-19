6b3c33e1f929db9dc630f8117cda47410b1ca0a2
Taleem <taleemmankuer@gmail.com>
2023-06-19 13:24:13 +0300

    add comments schema

---
+++ schemas/comments.js
@@ -0,0 +1,48 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "comment",
+  type: "document",
+  title: "Comment",
+  fields: [
+    defineField({
+      title: "Name",
+      name: "name",
+      type: "string",
+    }),
+    defineField({
+      title: "Approved",
+      name: "approved",
+      type: "boolean",
+      description: "Comments won't show on the site without approval",
+    }),
+    defineField({
+      title: "Email",
+      name: "email",
+      type: "string",
+    }),
+    defineField({
+      title: "Title",
+      name: "comment",
+      type: "text",
+    }),
+    defineField({
+      name: "post",
+      type: "reference",
+      to: [{ type: "post" }],
+    }),
+  ],
+  preview: {
+    select: {
+      name: "name",
+      comment: "comment",
+      post: "post.title",
+    },
+    prepare({ name, comment, post }) {
+      return {
+        title: `${name} on ${post}`,
+        subtitle: comment,
+      };
+    },
+  },
+});
--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,7 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
+import blockContent from "./blockContent";
+import category from "./category";
+import post from "./post";
+import author from "./author";
+import comments from "./comments";
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [post, author, category, comments, blockContent];
