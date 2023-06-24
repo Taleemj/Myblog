92965543d2d42cbe197a9bd40fb23e8721219a4a
Taleem <taleemmankuer@gmail.com>
2023-06-24 19:24:41 +0300

    add google analytics

--- app/(frontend)/head.js
+++ app/(frontend)/head.js
@@ -1,6 +1,27 @@
+import Script from "next/script";
 const head = () => {
   return (
     <>
+      <Script
+        async
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        async
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <title>The developer Blog</title>
       <meta
         name="description"
