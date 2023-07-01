44bd31b4063e28609789ef041a3521ccccce964b
Taleem <taleemmankuer@gmail.com>
2023-07-01 16:32:11 +0300

    update

--- app/(frontend)/post/[slug]/page.jsx
+++ app/(frontend)/post/[slug]/page.jsx
@@ -69,6 +69,16 @@ const Post = async ({ params: { slug } }) => {
         >
           <div overflow=""></div>
         </amp-ad> */}
+        <script
+          async="async"
+          data-cfasync="false"
+          src="//pl19914491.highrevenuegate.com/db5a30a7a3e08b7d754cc80feee53da7/invoke.js"
+        ></script>
+        <div id="container-db5a30a7a3e08b7d754cc80feee53da7"></div>
+        <script
+          defer
+          src="//servedby.studads.com/ads/ads.php?t=MTg5NTg7MTI3MDE7dmVydGljYWwuc2t5c2NyYXBlcg==&index=1"
+        ></script>
       </div>
       <main className={styles.container}>
         <div className={styles.blog}>
