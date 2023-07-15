b59819dff022a979c399e286a787a554dcdd0b5f
Taleem <taleemmankuer@gmail.com>
2023-07-15 17:25:40 +0300

    update studio metadata

--- app/(admin)/studio/[[...index]]/layout.jsx
+++ app/(admin)/studio/[[...index]]/layout.jsx
@@ -1,7 +1,17 @@
 "use client";
+// Re-export `NextStudioHead` as default if you're happy with the default behavior
+export { NextStudioHead } from "next-sanity/studio/head";
+
+// To customize it, use it as a children component:
+import { NextStudioHead } from "next-sanity/studio/head";
+
 export default function StudioLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <NextStudioHead favicons={false} />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+      </head>
       <body>{children}</body>
     </html>
   );
