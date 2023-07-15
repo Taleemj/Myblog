c39e562da00f9581d9cca51428cac3a8f37bba71
Taleem <taleemmankuer@gmail.com>
2023-07-15 17:14:52 +0300

    add metadata

--- app/(frontend)/aboutMe/page.jsx
+++ app/(frontend)/aboutMe/page.jsx
@@ -9,6 +9,12 @@ import { RichText } from "@/components/Richtext/RichText";
 
 export const revalidate = 60;
 
+export const metadata = {
+  title: "About Me",
+  description:
+    "the developer blog is a platform dedicated to sharing knowledge, experience, and insights on programming and technology-related topics. It aims to provide valuable information and resources to fellow developers, beginners, and enthusiasts alike. we covers a wide range of topics such as programming languages, web development, software engineering, algorithms, data structures, best practices, and industry news. It also features tutorials, code snippets, and practical examples that help readers understand complex concepts and apply them in real-world scenarios. The developer blog style is approachable, informative, and engaging, making it easy for readers to follow and learn. Whether you are a seasoned programmer or a beginner, it offers something for everyone who is interested in the world of programming and technology",
+};
+
 const page = async () => {
   const query = groq`
   *[_type == "author"][0]
