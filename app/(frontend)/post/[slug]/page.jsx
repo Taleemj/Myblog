3d36000176f18f6a18a8fc1d4b86d2b0b8c88ac0
Taleem <taleemmankuer@gmail.com>
2023-07-27 14:16:34 +0300

    update

--- app/(frontend)/post/[slug]/page.jsx
+++ app/(frontend)/post/[slug]/page.jsx
@@ -67,16 +67,16 @@ const Post = async ({ params: { slug } }) => {
     <div className={styles.main}>
       <ScrollBar styles={styles} />
       <div className={styles.ads1}>
-        {/* <script
+        <script
           async="async"
           data-cfasync="false"
-          src="//pl19914491.highrevenuegate.com/db5a30a7a3e08b7d754cc80feee53da7/invoke.js"
+          src="//pl19914491.highwaycpmrevenue.com/db5a30a7a3e08b7d754cc80feee53da7/invoke.js"
         ></script>
         <div id="container-db5a30a7a3e08b7d754cc80feee53da7"></div>
         <script
           defer
           src="//servedby.studads.com/ads/ads.php?t=MTg5NTg7MTI3MDE7dmVydGljYWwuc2t5c2NyYXBlcg==&index=1"
-        ></script> */}
+        ></script>
       </div>
       <main className={styles.container}>
         <div className={styles.blog}>
