56959c024ef2f2592636edd93ba0160c836214af
Taleem <taleemmankuer@gmail.com>
2023-07-29 12:13:30 +0300

    update

--- components/Ads.jsx
+++ components/Ads.jsx
@@ -3,17 +3,24 @@
 const Ads = () => {
   return (
     <div>
-      {/* <script type="text/javascript">
-	  { tOptions = {
-		'key' : '446cfc4ff35e39c76e4a08df1d3e63ca',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	},
-	  document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + 
-    '://www.profitabledisplaynetwork.com/446cfc4ff35e39c76e4a08df1d3e63ca/invoke.js"></scr' + 'ipt>'),}
-  </script> */}
+      <script type="text/javascript">
+        {
+          ((tOptions = {
+            key: "446cfc4ff35e39c76e4a08df1d3e63ca",
+            format: "iframe",
+            height: 90,
+            width: 728,
+            params: {},
+          }),
+          document.write(
+            "<scr" +
+              'ipt type="text/javascript" src="http' +
+              (location.protocol === "https:" ? "s" : "") +
+              '://www.profitabledisplaynetwork.com/446cfc4ff35e39c76e4a08df1d3e63ca/invoke.js"></scr' +
+              "ipt>"
+          ))
+        }
+      </script>
     </div>
   );
 };
