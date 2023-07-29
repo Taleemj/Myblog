d3d68874137181bb0ee6d0b8d1bcacb132408839
Taleem <taleemmankuer@gmail.com>
2023-07-29 13:39:23 +0300

    update

--- app/(frontend)/post/[slug]/page.jsx
+++ app/(frontend)/post/[slug]/page.jsx
@@ -103,24 +103,6 @@ const Post = async ({ params: { slug } }) => {
               />
             </div>
           </a>
-          <script type="text/javascript">
-            {
-              ((tOptions = {
-                key: "446cfc4ff35e39c76e4a08df1d3e63ca",
-                format: "iframe",
-                height: 90,
-                width: 728,
-                params: {},
-              }),
-              document.write(
-                "<scr" +
-                  'ipt type="text/javascript" src="http' +
-                  (location.protocol === "https:" ? "s" : "") +
-                  '://www.profitabledisplaynetwork.com/446cfc4ff35e39c76e4a08df1d3e63ca/invoke.js"></scr' +
-                  "ipt>"
-              ))
-            }
-          </script>
           <article>
             <PortableText value={post.body} components={RichText} />
             <p>
