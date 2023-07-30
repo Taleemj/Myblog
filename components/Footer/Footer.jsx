82126628ecc65ef083f87616026cce8530678a04
Taleem <taleemmankuer@gmail.com>
2023-07-30 12:07:41 +0300

    add firebase to store email subscriptions

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,13 +1,40 @@
+"use client";
 import styles from "./Footer.module.scss";
 import Link from "next/link";
-import Script from "next/script";
 import {
   AiOutlineLinkedin,
   AiFillGithub,
   AiOutlineHeart,
 } from "react-icons/ai";
+import { useRef, useState } from "react";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebaseconfig";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 
 const Footer = () => {
+  const emailRef = useRef();
+  const [loading, setloading] = useState(false);
+  const [submitted, setsubmitted] = useState(false);
+
+  const getUserEmail = async (e) => {
+    setloading(true);
+    e.preventDefault();
+    const emails = {};
+    const email = emailRef.current.value;
+    const newemail = email;
+    emails[`${email.substring(0, email.length - 4)}`] = newemail;
+    try {
+      const docRef = await updateDoc(
+        doc(db, "newsletterEmails", "emails"),
+        emails
+      );
+    } catch (error) {
+      console.error(error);
+    }
+    console.log(emails);
+    setloading(false);
+    setsubmitted(true);
+  };
   return (
     <div className={styles.container}>
       <ul className={styles.links}>
@@ -50,18 +77,30 @@ const Footer = () => {
         </div>
       </ul>
       <div className={styles.newsletter}>
-        <h4>Never miss a post</h4>
-        <p>Enter email here</p>
-        <form action="">
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Email Adress"
-            required
-          />
-          <button type="submit">Subscribe</button>
-        </form>
+        {!loading & !submitted ? (
+          <>
+            <h4>Never miss a post</h4>
+            <p>Enter email here</p>
+            <form onSubmit={getUserEmail}>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email Adress"
+                ref={emailRef}
+                required
+              />
+              <button type="submit">Subscribe</button>
+            </form>
+          </>
+        ) : loading & !submitted ? (
+          <h1>Submiting</h1>
+        ) : (
+          <div className={styles.submitted}>
+            <CheckCircleIcon />
+            <h1>Subscribed successfully</h1>
+          </div>
+        )}
         <p className={styles.copy}>
           <span className={styles.copy2}>
             2023 made with
--- components/Newsletter/Newsletter.jsx
+++ components/Newsletter/Newsletter.jsx
@@ -1,22 +1,63 @@
+"use client";
 import styles from "./Newsletter.module.scss";
-import Script from "next/script";
+import { useRef, useState } from "react";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "@/firebaseconfig";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 
 const Newsletter = () => {
+  const emailRef = useRef();
+  const [loading, setloading] = useState(false);
+  const [submitted, setsubmitted] = useState(false);
+
+  const getUserEmail = async (e) => {
+    setloading(true);
+    e.preventDefault();
+    const emails = {};
+    const email = emailRef.current.value;
+    const newemail = email;
+    emails[`${email.substring(0, email.length - 4)}`] = newemail;
+    try {
+      const docRef = await updateDoc(
+        doc(db, "newsletterEmails", "emails"),
+        emails
+      );
+    } catch (error) {
+      console.error(error);
+    }
+    console.log(emails);
+    // emailRef && emailRef.current.value == "";
+    setloading(false);
+    setsubmitted(true);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.txt}>
-        <h1>Never Miss a New Post.</h1>
-      </div>
-      <form action="">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Enter your Email *"
-          required
-        />
-        <button type="submit">Subscribe</button>
-      </form>
+      {!loading & !submitted ? (
+        <>
+          <div className={styles.txt}>
+            <h1>Never Miss a New Post.</h1>
+          </div>
+          <form onSubmit={getUserEmail}>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your Email *"
+              ref={emailRef}
+              required
+            />
+            <button type="submit">Subscribe</button>
+          </form>
+        </>
+      ) : loading & !submitted ? (
+        <h1>Submiting</h1>
+      ) : (
+        <div className={styles.submitted}>
+          <CheckCircleIcon />
+          <h1>Subscribed successfully</h1>
+        </div>
+      )}
     </div>
   );
 };
